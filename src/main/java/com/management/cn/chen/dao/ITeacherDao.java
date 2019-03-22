abbe6afc18d0f98527c45520a2c1d5807bb29747
a1114651313 <a1114651313@qq.com>
2019-03-22 08:45:56 +0800

    测试

---
+++ src/main/java/com/management/cn/chen/dao/ITeacherDao.java
@@ -0,0 +1,9 @@
+package com.management.cn.chen.dao;
+
+import com.management.cn.entity.Teacher;
+
+public interface ITeacherDao {
+
+    Teacher getTeacher(String username);
+
+}
---
+++ src/main/java/com/management/cn/chen/service/ITeacherService.java
@@ -0,0 +1,10 @@
+package com.management.cn.chen.service;
+
+
+import com.management.cn.entity.Teacher;
+
+public interface ITeacherService {
+
+    Teacher getTeacher(String username);
+
+}
